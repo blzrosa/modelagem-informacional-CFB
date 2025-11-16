3fe1145c284504586ad687d72a70861238874853
blzrosa <vblzrosa@gmail.com>
2025-11-16 16:57:36 -0300

    processei todas as fontes de dados

--- Fase2/DDL_DW_SCORE.sql
+++ Fase2/DDL_DW_SCORE.sql
@@ -15,6 +15,7 @@ CREATE TABLE dw_score.DimCidadePotencial (
 
 CREATE TABLE dw_score.DimPopulacao (
     KeyPopulacao INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    ContagemPop INT NOT NULL,
     FaixaEtariaPop VARCHAR(32) NOT NULL,
     SexoPop CHAR(1) NOT NULL,
     NomeCidadePopulacao VARCHAR(255) NOT NULL,
@@ -37,6 +38,16 @@ CREATE TABLE dw_score.DimContagemFarmacias (
     CodigoIBGE CHAR(7) NOT NULL
 );
 
+CREATE TABLE dw_score.DimPIB (
+    KeyPIB INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    ValorPIB INT,
+    AnoPIB INT NOT NULL,
+    SetorPIB VARCHAR(32) NOT NULL,
+    NomeCidadePIB VARCHAR(255) NOT NULL,
+    UFPIB CHAR(2) NOT NULL,
+    CodigoIBGE CHAR(7) NOT NULL
+)
+
 /*
 ============
 ==  FATO  ==
@@ -46,11 +57,13 @@ CREATE TABLE dw_score.FatoScore (
     ValorScore FLOAT NOT NULL,
     ContagemFarmacias INT NOT NULL CHECK(ContagemFarmacias >= 0),
     PopulacaoTotal INT NOT NULL CHECK(PopulacaoTotal >= 0),
+    AnoScore INT NOT NULL CHECK(AnoScore >= 0),
 
     KeyCidadePotencial NOT NULL REFERENCES dw_score.DimCidadePotencial(KeyCidadePotencial),
     KeyPopulacao NOT NULL REFERENCES dw_score.DimPopulacao(KeyPopulacao),
     KeyPesoFaixaEtaria NOT NULL REFERENCES dw_score.DimPesoFaixaEtaria(KeyPesoFaixaEtaria),
     KeyContagemFarmacias NOT NULL REFERENCES dw_score.DimContagemFarmacias(KeyContagemFarmacias),
+    KeyPIB NOT NULL REFERENCES dw_score.DimPIB(KeyPIB),
 
-    PRIMARY KEY (KeyCidadePotencial, KeyPopulacao, KeyPesoFaixaEtaria)
+    PRIMARY KEY (KeyCidadePotencial, KeyPopulacao, KeyPesoFaixaEtaria, KeyPIB)
 )
